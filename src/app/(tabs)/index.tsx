41044d74946a48b3044b789e7747a656497f469c
Kevin Aguilar <kevinaguilar0813@gmail.com>
2026-02-25 13:25:50 -0600

    refactor: move app to src/app and updated path alias

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -12,9 +12,7 @@ export default function TabOneScreen() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
+    flex: 1
   },
   title: {
     fontSize: 20,
