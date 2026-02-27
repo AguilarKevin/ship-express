16d21ed1c7ae8348492e999f8d257165bbbc6c04
Kevin Aguilar <kevinaguilar0813@gmail.com>
2026-02-26 22:06:47 -0600

    feat: add themed SafeAreaView

--- src/components/Themed.tsx
+++ src/components/Themed.tsx
@@ -2,9 +2,14 @@
  * Themed components using Tamagui's theme system
  */
 import {
+  SafeAreaView as DefaultSafeAreaView,
+  SafeAreaViewProps
+} from "react-native-safe-area-context";
+import {
   ScrollView as DefaultScrollView,
   Text as DefaultText,
   View as DefaultView,
+  useTheme,
   type ScrollViewProps,
   type TextProps,
   type ViewProps
@@ -20,4 +25,17 @@ export function View(props: ViewProps) {
 
 export function ScrollView(props: ScrollViewProps) {
   return <DefaultScrollView bg="$background" {...props} />;
+}
+
+export function SafeAreaView(props: SafeAreaViewProps) {
+  const { style, ...rest } = props;
+
+  const theme = useTheme();
+
+  return (
+    <DefaultSafeAreaView
+      style={[{ flex: 1, backgroundColor: theme.background.val }, style]}
+      {...rest}
+    />
+  );
 }
